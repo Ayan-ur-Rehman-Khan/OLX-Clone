9076e25916f98105f3e4c0c9bf4bd4b5b12e8b53
Ayan Ur Rehman Khan <ayanurehmank@gmail.com>
2025-07-26 15:15:37 +0500

    With Login And Register Page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 async function getData() {
     let response = await fetch("https://dummyjson.com/products");
     let data = await response.json();
@@ -31,3 +32,110 @@ async function getData() {
 }
 
 getData();
+
+const model = document.getElementById("model")
+const loginbtn = document.getElementById("log")
+const main = document.getElementById("main")
+const log2 = document.getElementById("log2")
+const profile = document.getElementById("profile")
+const avatar = document.getElementById("avatar")
+const logout = document.getElementById("logoutBtn")
+const username = document.getElementById("profile-name")
+const useremail = document.getElementById("profile-email")
+const info = document.getElementById("info")
+
+loginbtn.onclick = () => {
+    model.style.display = "block"
+    model.style.zIndex = "9999"
+    model.style.position = "fixed"
+    main.style.display = "none"
+}
+
+log2.onclick = () => {
+    model.style.display = "none"
+    main.style.display = "block"
+    loginbtn.style.display = "none"
+    avatar.style.display = "block"
+
+
+
+}
+
+logout.onclick = () => {
+    avatar.style.display = "none"
+    loginbtn.style.display = "block"
+}
+
+
+
+class Person {
+    fullName
+    email
+    password
+    constructor(fullName, email, password) {
+        this.fullName = fullName,
+            this.email = email,
+            this.password = password
+    }
+}
+
+let users = []
+let userName = document.getElementById("username")
+let userEmail = document.getElementById("useremail")
+
+function registerUser(event) {
+    event.preventDefault()
+    let fullName = document.getElementById("fullName")
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+
+    let usersFromStorage = JSON.parse(localStorage.getItem("users"))
+    let savedUser = usersFromStorage.find((element) => element.email === email.value)
+
+    if (savedUser?.email) {
+        alert("user already register he ")
+    } else {
+        let newUser = new Person(fullName.value, email.value, password.value)
+        users.push(newUser)
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+
+    fullName.value = ""
+    email.value = ""
+    password.value = ""
+}
+
+
+function loginUser(event) {
+    event.preventDefault()
+    let email = document.getElementById("loginEmail")
+    let password = document.getElementById("loginPassword")
+    let usersFromStorage = JSON.parse(localStorage.getItem("users"))
+    let savedUser = usersFromStorage.find((element) => element.email === email.value)
+
+    if (savedUser?.email === email.value && savedUser?.password === password.value) {
+        alert("you have logged in successfully!")
+        localStorage.setItem("loggedinUser", JSON.stringify(savedUser))
+        // userName.innerHTML = savedUser.fullName
+        // userEmail.innerHTML = savedUser.email
+        avatar.style.display = "block"
+        // document.getElementById("profile-name").innerText = savedUser.fullNamename;
+        // document.getElementById("profile-email").innerText = savedUser.email;
+        username.innerHTML = savedUser.fullName
+        useremail.innerHTML = savedUser.email
+        console.log(savedUser.fullName)
+
+    } else {
+        alert("Invalid credientials")
+    }
+
+}
+
+function logoutUser() {
+    localStorage.removeItem("loggedinUser")
+    username.innerHTML = ''
+    useremail.innerHTML = ''
+
+}
+
+
